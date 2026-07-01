c53327d447d36417fb2dc6d4d7ab01387b456742
jdethington <jdethington@gmail.com>
2026-07-01 13:17:25 -0700

    Fixed Items not showing up in the cart

--- src/js/product.js
+++ src/js/product.js
@@ -4,8 +4,11 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const itemsInCart = JSON.parse(localStorage.getItem("so-cart")) || [];
+  itemsInCart.push(product);
+  setLocalStorage("so-cart", itemsInCart);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
