9ee771e67160a555684b22efef34d5431bb78173
kimchristian50 <pchristison@byupathway.edu>
2026-07-01 22:29:42 -0600

    edited product.js to allow multiple items to be added to the cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,19 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // get the current cart contents, or start with an empty cart if nothing is there yet
+  let cart = getLocalStorage("so-cart");
+  if (!Array.isArray(cart)) {
+    cart = [];
+  }
+  // add the new product to our array
+  cart.push(product);
+
+  // save the updated array back to local storage
+  setLocalStorage("so-cart", cart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
