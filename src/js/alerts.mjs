0e529d6f64896600b0d6a3d836a1ad50e11d95a4
jdethington <jdethington@gmail.com>
2026-07-13 14:09:48 -0700

    Created alert to main listing

--- src/js/alerts.mjs
+++ src/js/alerts.mjs
@@ -3,28 +3,35 @@ export default class Alerts {
         this.loadAlerts();
     }
     
-    async loadAlerts() {
-        const response = await fetch("../json/alerts.json");
-        const alerts = await response.json();
-        if (alerts.length > 0) {
-        this.createAlertSection(alerts);            
-        }
+  async loadAlerts() {
+    try {
+      const response = await fetch("../json/alerts.json");
+      if (!response.ok) {
+        throw new Error(`Error! Status ${response.status}`);
+      }
+      const alerts = await response.json();
+      if (alerts.length > 0) {
+        this.createAlertSection(alerts);
+      }
+    } catch (error){
+        console.error("There was a problem fetching the data: ", error)
     }
+  }
 
-    createAlertSection(alerts) {
-    const section = document.createElement("section");
-    section.className = "alert-list";
-    
-    alerts.forEach(alert => {
-      const p = document.createElement("p");
-      p.textContent = alert.message;
-      p.style.backgroundColor = alert.background;
-      p.style.color = alert.color;
-      section.appendChild(p);
-    });
-    
-    // Prepend to main element (or body as fallback)
-    const main = document.querySelector("main");
-      main.prepend(section);
+  createAlertSection(alerts) {
+  const section = document.createElement("section");
+  section.className = "alert-list";
+  
+  alerts.forEach(alert => {
+    const p = document.createElement("p");
+    p.textContent = alert.message;
+    p.style.backgroundColor = alert.background;
+    p.style.color = alert.color;
+    section.appendChild(p);
+  });
+  
+  // Prepend to main element
+  const main = document.querySelector("main");
+    main.prepend(section);
   }
 }
