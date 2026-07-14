ac86991a4eea90430b66810c2a19f1a2eeaa13f9
jdethington <jdethington@gmail.com>
2026-07-13 17:17:33 -0700

    Step 2

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
-import ProductData from "./ProductData.mjs";
-import ProductList from "./ProductList.mjs";
+// import ProductData from "./ProductData.mjs";
+// import ProductList from "./ProductList.mjs";
 
-const dataSource = new ProductData("tents");
-const element = document.querySelector(".product-list");
+// const dataSource = new ProductData("tents");
+// const element = document.querySelector(".product-list");
 
-const productList = new ProductList("tents", dataSource, element);
+// const productList = new ProductList("tents", dataSource, element);
 
-productList.init();
+// productList.init();
