7aa1a748d00244729e73a7e0ad116dcac0b37d38
jdethington <jdethington@gmail.com>
2026-07-16 19:30:29 -0700

    set a timer to wait to load alerts

--- src/js/main.js
+++ src/js/main.js
@@ -5,11 +5,14 @@ import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
 
-new Alerts();
-
 const dataSource = new ProductData("tents");
 const element = document.querySelector(".product-list");
 
 const productList = new ProductList("tents", dataSource, element);
 
 productList.init();
+
+setTimeout(alert, 5000);
+function alert() {
+  new Alerts();
+}
