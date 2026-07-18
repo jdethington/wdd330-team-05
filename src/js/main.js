36cd5f652dca865a795bebdff08740506d03f1a3
kimchristian50 <pchristison@byupathway.edu>
2026-07-17 22:17:27 -0600

    individual week 3

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,13 @@ import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
 
+import { cartSuperscript } from "./utils.mjs";
+cartSuperscript();
+
+
 // const dataSource = new ProductData("tents");
 // const element = document.querySelector(".product-list");
 
 // const productList = new ProductList("tents", dataSource, element);
 
 // productList.init();
-import { cartSuperscript } from "./utils.mjs";
-cartSuperscript();
